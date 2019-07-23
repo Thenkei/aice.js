afdb3310263ca499abdd1fa0f18525374c907329
Morgan Perre <morgan.perre@gmail.com>
2019-07-23 17:16:48 +0200

    Hamming distance - Basic implementation
    + This might be used in a new strategy (Need to change the way it works distanceH(a,b)===distance(b,a)

---
+++ src/utils/comparator/hamming.js
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2015-present, CWB SAS
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = (a, b) => {
+  if (!a || !b) {
+    return 0;
+  }
+  if (a.length > b.length) {
+    throw new Error('Hamming - Length of strings a must be smaller or equal to b');
+  }
+
+  let distance = 0;
+  for (let i = 0; i < a.length; i += 1) {
+    if (a[i] !== b[i]) {
+      distance += 1;
+    }
+  }
+  distance += b.length - a.length;
+  return distance;
+};
---
+++ test/utils/hamming.test.js
@@ -0,0 +1,24 @@
+const chai = require('chai');
+
+const { expect } = chai;
+
+const hammingDistance = require('../../src/utils/comparator/hamming');
+
+describe('Hamming distance', () => {
+  const baseString = 'antidisestablishmentarianism';
+  it('Hamming distance - from nothing', () => {
+    const distance = hammingDistance();
+    expect(distance).to.equal(0);
+  });
+
+  it('Hamming distance - just one parameter', () => {
+    const distance = hammingDistance(baseString, 'moredisestablishmentarianism');
+    expect(distance).to.equal(4);
+  });
+
+  it('Hamming distance - should throw error', () => {
+    expect(() => hammingDistance(baseString, 'menta')).to.throw(
+      'Hamming - Length of strings a must be smaller or equal to b',
+    );
+  });
+});
